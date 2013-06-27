3be94532f3a56f16d6931b9a65cad104ad1a190e
Jason O'Neil <jason.oneil@gmail.com>
2013-06-27 21:04:33 +0800

    Move server to it's own command, add some config options

--- src/ufront/tool/UFTool.hx
+++ src/ufront/tool/UFTool.hx
@@ -58,25 +58,8 @@ class UFTool extends CommandLine {
         Run a development server using "nekotools server" on localhost:2987
         @alias s
     **/
-    public function server() {
-        var serverName = Host.localhost().toString();
-        var port = 2987;
-        var webDir = "out";
-
-        if ( sys.FileSystem.exists(webDir) ) {
-            println('Switching directory to `$webDir`');
-            var originalCwd = getCwd();
-            setCwd(webDir);
-            
-            Sys.command("nekotools", ["server", "-rewrite", "-h", serverName, "-p", ""+port]);
-            
-            setCwd(originalCwd);
-            exit(0);
-        }
-        else {
-            println('Failed to change into directory $webDir');
-            exit(1);
-        }
+    public function server( d:Dispatch ) {
+        d.dispatch(new ServerCommand());
     }
     
     /**
@@ -298,5 +281,66 @@ class ShellCommand extends UfrontCommand
         
         Sys.command("haxelib", args);
         exit(0);
+    }
+}
+
+
+/**
+    -------------------------------------------------------------------
+    Ufront Tool:
+      ufront server
+    
+    Run a local development server (using `nekotools server`).
+    -------------------------------------------------------------------
+
+    Usage:
+**/
+class ServerCommand extends UfrontCommand
+{
+    /** 
+        The port to run the server on.  `2987` by default.
+    **/
+    public var port:Int = 2987;
+    
+    /** 
+        The directory to launch the server in.  "Out" by default.
+    **/
+    public var dir:String = "out";
+    
+    /** 
+        Server name to listen on.  Can be an IP address.  Will be the value of `Host.localhost().toString()` by default.
+    **/
+    public var host:String = null;
+    
+    /** 
+        Flag to disable URL rewriting on server  (URL rewriting is enabled by default)
+    **/
+    public var norewrite:Bool = false;
+
+    public function runDefault()
+    {
+        if ( host==null ) host = Host.localhost().toString();
+
+        if ( sys.FileSystem.exists(dir) ) {
+            println('Switching directory to `$dir`');
+            var originalCwd = getCwd();
+            setCwd(dir);
+
+            var args = ["server"];
+            args.push( "-h" );
+            args.push( host );
+            args.push( "-p" );
+            args.push( ''+port );
+            if ( !norewrite ) args.push( "-rewrite" );
+
+            Sys.command("nekotools", args);
+            
+            setCwd(originalCwd);
+            exit(0);
+        }
+        else {
+            println('Failed to change into directory $dir');
+            exit(1);
+        }
     }
 }
