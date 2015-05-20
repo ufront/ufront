60c690b7d83093889866af9ec7b0fce15f524f73
Jason O'Neil <jason.oneil@gmail.com>
2015-05-20 19:28:53 +0800

    Add submodules for ufront dependencies, so we can track dev versions more easily.
    I've also added a `setupDev.sh` shell script, which (when run in the ufront repo)
    will set the dev paths to all of the haxelibs.
    
    I might make a more robust solution later.

---
+++ setupDev.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+haxelib dev ufront .
+haxelib dev minject submodules/minject/src
+haxelib dev ufront-client submodules/ufront-client
+haxelib dev ufront-easyauth submodules/ufront-easyauth
+haxelib dev ufront-mail submodules/ufront-mail
+haxelib dev ufront-mvc submodules/ufront-mvc
+haxelib dev ufront-orm submodules/ufront-orm
+haxelib dev ufront-ufadmin submodules/ufront-ufadmin
+haxelib dev ufront-uftasks submodules/ufront-uftasks
